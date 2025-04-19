bfd6bb2ea6e8ba6de470019b6f76f421e9f9c8b7
Harakirigi <nkomyt2@gmail.com>
2025-04-20 02:52:37 +0500

    version 0.1

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <>
     <div className="p-8 space-y-4">
-        <div className="p-4 text-white bg-accent-10 text-shadow-transparent">bg-accent</div>
-        <div className="p-4 text-black bg-accent-light">bg-accent-light</div>
+        <div className="p-4 text-white bg-accent-50 text-shadow-transparent">bg-accent</div>
+        <div className="p-4 text-secondary bg-accent">bg-accent-light</div>
         <div className="p-4 text-white bg-accent-dark">bg-accent-dark</div>
     </div>
 </>
