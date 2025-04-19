ab7055926e3359f777e2210054e60b4cab85b6b8
Harakirigi <nkomyt2@gmail.com>
2025-04-20 01:30:03 +0500

    version 0.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
     content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
+        './src/app/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-      extend: {
-        colors: {
-            brand: {
-                light: '#3AB0FF',
-                DEFAULT: '#0081CF',
-                dark: '#005B99'
+        extend: {
+            colors: {
+                accent: {
+                    light: '#28CFBD',
+                    DEFAULT: '#10AAA6',
+                    dark: '#090B0C',
+                },
             },
             fontFamily: {
                 sans: ['Inter', 'sans-serif'],
-                funky: ['"Comic Sans MS"', 'cursive']
+                funky: ['"Comic Sans MS"', 'cursive'],
             },
             spacing: {
                 '72': '18rem',
                 '84': '21rem',
-                '96': '24rem'
+                '96': '24rem',
             },
             screens: {
-                'xs': '480px',
-                '3xl': '1920px'
-            }
-        }
-      },
+                xs: '480px',
+                '3xl': '1920px',
+            },
+        },
     },
     plugins: [],
-  }
-  
+}
+
+export default config
