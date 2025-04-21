9846d418fd1f7611609a45f6cd5da2539f69fefe
Harakirigi <nkomyt2@gmail.com>
2025-04-21 15:52:43 +0500

    version 0.1

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,13 +3,18 @@
 import PropTypes from "prop-types"
 
 const Button = ({
-    className,
-    variant,
-    color,
+
+    className = '',
+    variant = 'primary',
     children,
+
 }) => {
+
+    const baseClass = ''
+
+
     return (
-        <button className={`btn ${variant} ${className} `}>
+        <button className={`w-full max-w-max min-w-[150px] h-11 border border-transparent rounded-lg font-sans font-medium text-base transition-all duration-300 ${variant} ${className}`}>
             {children}
         </button>
     )
