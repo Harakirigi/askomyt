1c2c1d4119b3ec7a910f4c19eef45a6da05e43df
Harakirigi <nkomyt2@gmail.com>
2025-05-06 18:51:17 +0500

    version 0.1

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,11 +6,6 @@ export default function Home() {
   return (
     <>
     <div className="p-8 space-y-4">
-        <div className="p-4 font-thin text-white bg-accent-50 text-shadow-transparent">bg-accent</div>
-        <div className="p-4 font-extrabold bg-black text-secondary">bg-accent-light</div>
-        <div className="p-4 font-mono text-white bg-accent-dark">ASASASA</div>
-        <Button variant={'primary'} icon={<Box></Box>}>adfjksdjhfdsh</Button>
-
     </div>
 </>
     
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+
+export const Header = () => {
+    <header className="w-dvw h-dvh bg-white-100">
+        <div>
+
+            <Link href={"/"}>About Me</Link>
+            <Link href={"/"}>My Projects</Link>
+            <Link href={"/"}>My Contects</Link>
+
+        </div>
+    </header>
+}
